593e8fbcfd835e1dd7fc23dc7b700a175366b854
gmajors1326 <gmajors1326@gmail.com>
2026-01-29 17:30:05 -0600

    revert: Remove explicit output config from next.config.js to align with Vercel serverless deployment expectations

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Configure output to 'export' to generate static HTML/CSS/JS in the 'out' directory.
-  output: 'export', 
 };
 
 module.exports = nextConfig;
