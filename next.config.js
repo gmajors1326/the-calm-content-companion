56822b064bba7b1a790f0da0952472aec01701f7
gmajors1326 <gmajors1326@gmail.com>
2026-01-29 17:41:02 -0600

    cleanup: Remove next.config.js and revert build script to default to resolve Vercel output directory errors

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Explicitly set output to 'export' to generate static HTML/CSS/JS.
-  output: 'export', 
-};
-
-module.exports = nextConfig;
