0b0a2147ca954467e31cf1aac399e8f8992a47e6
gmajors1326 <gmajors1326@gmail.com>
2026-01-29 17:30:45 -0600

    fix: Force static build artifacts into public/ directory to resolve Vercel missing output directory error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Explicitly set output to 'export' to generate static HTML/CSS/JS.
+  output: 'export', 
 };
 
 module.exports = nextConfig;
